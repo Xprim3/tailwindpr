7fef584a8fe348fdaca94a2ea6e7ff3c803ed59f
Denis Januzi <denisjanuzi@gmail.com>
2025-05-12 15:05:15 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import './style.css'
+
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
